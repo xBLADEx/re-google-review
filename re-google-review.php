ffaec7745b8ce47e532c1e08a9da83cdeebbb29a
Rich Edmunds <rich@webdevstudios.com>
2016-12-06 20:57:48 -0700

    Setup plugin file.

---
+++ re-google-review.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: RE Google Review
+ * Plugin URI: http://www.richedmunds.com
+ * Description: Leave a Google review.
+ * Author: Rich Edmunds
+ * Author URI: http://www.richedmunds.com
+ * Version: 1.0
+ *
+ * @package RE Google Review
+ */
