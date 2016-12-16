697ec3ec5bcd194c1b0f7bdeee0fe42dc5452ef2
Rich Edmunds <rich@webdevstudios.com>
2016-12-15 22:06:43 -0700

    Use the title from the CPT to create unique review link.

--- assets/template/single.php
+++ assets/template/single.php
@@ -7,11 +7,8 @@
 
 ?>
 <?php get_header(); ?>
-<?php
-$place_id = get_option( 're_google_review_place_id' );
-?>
 <div class="open-review">
 	<h2>Use <span>Google</span><br>to leave your review?</h2>
-	<a href="http://search.google.com/local/writereview?placeid=<?php echo esc_attr( $place_id ); ?>" class="button">Yes</a>
+	<a href="http://search.google.com/local/writereview?placeid=<?php the_title(); ?>" class="button">Yes</a>
 </div>
 <?php get_footer(); ?>
