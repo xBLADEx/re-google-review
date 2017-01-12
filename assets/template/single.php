0b827758042f0f9716662b7cea9644a07c308866
Rich Edmunds <rich@webdevstudios.com>
2017-01-11 21:00:39 -0700

    Update Google Place ID to grab from new meta box rather than title.

--- assets/template/single.php
+++ assets/template/single.php
@@ -7,8 +7,11 @@
 
 ?>
 <?php get_header(); ?>
+<?php
+$google_review = get_post_meta( $post->ID, 're-google-review-google-place-id', true );
+?>
 <div class="open-review">
 	<h2>Use <span>Google</span><br>to leave your review?</h2>
-	<a href="http://search.google.com/local/writereview?placeid=<?php the_title(); ?>" class="button">Yes</a>
+	<a href="http://search.google.com/local/writereview?placeid=<?php echo esc_html( $google_review ); ?>" class="button">Yes</a>
 </div>
 <?php get_footer(); ?>
