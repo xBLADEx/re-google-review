445c554a000252c1f5746a54d7b4db7acc31921d
Rich Edmunds <rich@webdevstudios.com>
2017-01-11 20:40:33 -0700

    Add meta box fields for custom post type.

--- re-google-review.php
+++ re-google-review.php
@@ -18,8 +18,6 @@ define( 'RE_BASE_DIR', dirname( RE_BASE_FILE ) );
 // This plugin_dir_url includes the trailing slash.
 define( 'RE_PLUGIN_URL', plugin_dir_url( RE_BASE_FILE ) );
 
-
-
 // Enqueue Scripts.
 if ( ! is_admin() ) {
 	wp_enqueue_style( 're-google-review', RE_PLUGIN_URL . 'assets/css/review.css', '', '1.0' );
@@ -216,3 +214,153 @@ function re_google_review_template_select( $template ) {
 }
 
 add_filter( 'template_include', 're_google_review_template_select' );
+
+// Meta box variables.
+// See http://www.deluxeblogtips.com/how-to-create-meta-box-wordpress-post/.
+$prefix = 're_google_review_';
+
+$meta_box = array(
+	'id'       => 're-google-review-meta-box',
+	'title'    => 'Company Review Links',
+	'page'     => 're-google-reviews',
+	'context'  => 'normal',
+	'priority' => 'high',
+	'fields'   => array(
+		array(
+			'name' => 'Text box',
+			'desc' => 'Enter something here',
+			'id' => $prefix . 'text',
+			'type' => 'text',
+			'std' => 'Default value 1',
+		),
+		array(
+			'name' => 'Textarea',
+			'desc' => 'Enter big text here',
+			'id' => $prefix . 'textarea',
+			'type' => 'textarea',
+			'std' => 'Default value 2',
+		),
+		array(
+			'name' => 'Select box',
+			'id' => $prefix . 'select',
+			'type' => 'select',
+			'options' => array( 'Option 1', 'Option 2', 'Option 3' ),
+		),
+		array(
+			'name' => 'Radio',
+			'id' => $prefix . 'radio',
+			'type' => 'radio',
+			'options' => array(
+				array( 'name' => 'Name 1', 'value' => 'Value 1' ),
+				array( 'name' => 'Name 2', 'value' => 'Value 2' ),
+			),
+		),
+		array(
+			'name' => 'Checkbox',
+			'id' => $prefix . 'checkbox',
+			'type' => 'checkbox',
+		),
+	),
+);
+
+/**
+ * Add Meta Box
+ * Add meta box on re-google-reviews post type.
+ */
+function re_google_review_add_meta_box() {
+	global $meta_box;
+
+	add_meta_box( $meta_box['id'], $meta_box['title'], 're_google_review_meta_box_fields', $meta_box['page'], $meta_box['context'], $meta_box['priority'] );
+}
+
+add_action( 'admin_menu', 're_google_review_add_meta_box' );
+
+/**
+ * Show Meta Box Fields
+ */
+function re_google_review_meta_box_fields() {
+	global $meta_box, $post;
+
+	// Use nonce for verification.
+	echo '<input type="hidden" name="re_google_review_meta_box_nonce" value="', wp_create_nonce( basename( RE_BASE_FILE ) ), '" />';
+
+	echo '<table class="form-table">';
+
+	foreach ( $meta_box['fields'] as $field ) {
+		// Get current post meta data.
+		$meta = get_post_meta( $post->ID, $field['id'], true );
+
+		echo '<tr>',
+		'<th style="width:20%"><label for="', $field['id'], '">', $field['name'], '</label></th>',
+		'<td>';
+		switch ( $field['type'] ) {
+			case 'text':
+			echo '<input type="text" name="', $field['id'], '" id="', $field['id'], '" value="', $meta ? $meta : $field['std'], '" size="30" style="width:97%" />', '<br />', $field['desc'];
+			break;
+			case 'textarea':
+			echo '<textarea name="', $field['id'], '" id="', $field['id'], '" cols="60" rows="4" style="width:97%">', $meta ? $meta : $field['std'], '</textarea>', '<br />', $field['desc'];
+			break;
+			case 'select':
+			echo '<select name="', $field['id'], '" id="', $field['id'], '">';
+			foreach ( $field['options'] as $option ) {
+				echo '<option ', $meta == $option ? ' selected="selected"' : '', '>', $option, '</option>';
+			}
+			echo '</select>';
+			break;
+			case 'radio':
+			foreach ( $field['options'] as $option ) {
+				echo '<input type="radio" name="', $field['id'], '" value="', $option['value'], '"', $meta == $option['value'] ? ' checked="checked"' : '', ' />', $option['name'];
+			}
+			break;
+			case 'checkbox':
+			echo '<input type="checkbox" name="', $field['id'], '" id="', $field['id'], '"', $meta ? ' checked="checked"' : '', ' />';
+			break;
+		}
+		echo '</td><td>',
+		'</td></tr>';
+	}
+
+	echo '</table>';
+}
+
+/**
+ * Save Meta Box
+ * Save data from meta box.
+ *
+ * @param  integer $post_id Post ID.
+ */
+function re_google_review_save_meta_data( $post_id ) {
+	global $meta_box;
+
+	// Verify nonce.
+	if ( ! wp_verify_nonce( $_POST['re_google_review_meta_box_nonce'], basename( RE_BASE_FILE ) ) ) {
+		return $post_id;
+	}
+
+	// Check autosave.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return $post_id;
+	}
+
+	// Check permissions.
+	if ( 'post' === $_POST['post_type'] ) {
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return $post_id;
+		}
+	} elseif ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return $post_id;
+	}
+
+	foreach ( $meta_box['fields'] as $field ) {
+		$old = get_post_meta( $post_id, $field['id'], true );
+		$new = $_POST[$field['id']];
+
+		if ( $new && $new !== $old ) {
+			update_post_meta( $post_id, $field['id'], $new );
+		} elseif ( '' === $new && $old ) {
+			delete_post_meta( $post_id, $field['id'], $old );
+		}
+	}
+}
+
+add_action( 'save_post', 're_google_review_save_meta_data' );
